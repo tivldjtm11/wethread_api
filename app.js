7baa9c6471b9376a1de7c332edfe68051108e819
LEE HAEMIN <dlgoals3384@gmail.com>
2023-10-15 17:12:55 +0900

    layerd pattern 적용하여 CRUD 구현

---
+++ app.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
+const routes = require("./routes");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(routes);
+
+const server = http.createServer(app);
+const port = process.env.PORT 
+
+const start = async() => {
+    try{
+        server.listen(port, () => {
+            console.log(`Server is listening on ${port}`);
+        })
+    }catch(err){
+        console.error(err);
+    }
+}
+
+start();
+
+
+
+
+
